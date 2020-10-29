8e131a39b53aaadc8b9050625dcfb599c6839079
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-29 15:57:00 +0100

    Adding new functions

--- public/bandersnatch/src/videoComponent.js
+++ public/bandersnatch/src/videoComponent.js
@@ -10,5 +10,35 @@ class VideoComponent {
     });
 
     player.addChild(modal);
+
+    player.on('play', () => modal.close())
+
+    this.modal = modal
+  }
+
+  configureModal(selected) {
+    const modal = this.modal
+    modal.on('modalopen', this.getModalTemplate(selected, modal))
+    modal.open()
+  }
+
+  getModalTemplate(options, modal) {
+    return (_) => {
+      const [ option1, option2 ] = options
+      const htmlTemplate = `
+        <div class='overlay'>
+          <div class='videoButtonWrapper'>
+            <button class='btn btn-dark' onClick="window.nextChunk('${option1}')">
+              ${option1}
+            </button>
+            <button class='btn btn-dark' onClick="window.nextChunk('${option2}')">
+              ${option2}
+            </button>
+          </div>
+        </div>
+      `
+
+      modal.contentEl().innerHTML = htmlTemplate
+    }
   }
 }
