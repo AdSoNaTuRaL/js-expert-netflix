73eef6e9515e54ec9aa0850854f2f014c1182d53
adsonatural <adsonhenriquesilva@gmail.com>
2020-10-29 15:57:11 +0100

    Updating code

--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,10 +1,12 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON, network }) {
+  constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON
     this.network = network
+    this.videoComponent = videoComponent
 
     this.videoElement = null
     this.sourceBuffer = null
+    this.activeItem = {}
     this.selected = {}
     this.videoDuration = 0
   }
@@ -37,7 +39,44 @@ class VideoMediaPlayer {
       // evita rodar como LIVE
       mediaSource.duration = this.videoDuration
       await this.fileDownload(selected.url)
+      setInterval(this.waitForQuestions.bind(this), 200)
     }
+  }
+
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime)
+    const option = this.selected.at === currentTime
+    if (!option) return;
+
+    // evita que o modal seja aberto 2x no mesmo segundo
+    if (this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options)
+    this.activeItem = this.selected
+  }
+
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144
+    const prepareUrl = {
+      url: this.manifestJSON.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    }
+
+  }
+
+  async nextChunk(data) {
+    const key = data.toLowerCase()
+    const selected = this.manifestJSON[key]
+    this.selected = {
+      ...selected,
+      // ajusta o tempo que o modal vai aparecer, baseado no tempo corrente
+      at: parseInt(this.videoElement.currentTime + selected.at)
+    }
+
+    // deixa o restante do video rodar enquanto baixa o novo video
+    this.videoElement.play()
+    await this.fileDownload(selected.url)
   }
 
   async fileDownload(url) {
@@ -57,7 +96,7 @@ class VideoMediaPlayer {
   setVideoPlayerDuration(finalURL) {
     const bars = finalURL.split('/')
     const [ name, videoDuration ] = bars[bars.length - 1].split('-')
-    this.videoDuration += videoDuration
+    this.videoDuration += parseFloat(videoDuration)
   }
 
   async processBufferSegments(allSegments) {
